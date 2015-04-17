9a186c5931b48dba1fac1296f21b5ee4fa25b83e
Hang Cui <oscarcuihang@hotmail.com>
2015-04-16 20:35:29 -0500

    change phrasing lines

--- Æffect/AEStories.swift
+++ Æffect/AEStories.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class AEStories: NSObject {
-    var story = Array<AEStory>()
+    
+    var story = [AEStory]()
     
     func load(fromURLString: String, completionHandler: (AEStories, String?) -> Void) {
-        story = Array<AEStory>()
+        
+        story =  [AEStory]()
         if let url = NSURL(string: fromURLString) {
             let urlRequest = NSMutableURLRequest(URL: url)
             let session = NSURLSession.sharedSession()
@@ -36,27 +38,21 @@ class AEStories: NSObject {
     }
     
     func parse(jsonData: NSData, completionHandler: (AEStories, String?) -> Void) {
-        var jsonError: NSError?
         
+        var jsonError: NSError?
         if let jsonResult = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary {
-            //println(jsonResult.count)
             if (jsonResult.count > 0)
             {
-                NSLog("We are a go!")
-                //if let status = jsonResult["status"] as? NSString {
-                // if (status == "OK")
-                //{
-                
                 if let storyData = jsonResult["response"] as? NSDictionary {
-                    
-                    if let documents = storyData["docs"] as? NSArray{
-                        println(documents.count)
-                        if(documents.count==0){
+                    if let  docs = storyData["docs"] as? NSArray{
+                        println(docs.count)
+                        if(docs.count == 0){
                             println("no articles")
                         }
                         else{
-                            for specificStory in documents{
-                                println(specificStory["anger"])
+                            println("abcde")
+                            for specificStory in docs{
+                                //println(specificStory["anger"])
                                 
                                 let article_key = specificStory["article_id"] as? NSString
                                 let title = specificStory["title"] as? NSString
@@ -66,8 +62,37 @@ class AEStories: NSObject {
                                 let content_without_tags = specificStory["content_without_tags"] as? NSString
                                 let pubdate = specificStory["pubdate"] as? NSString
                                 let picture_url = specificStory["picture_url"] as? NSString
-                                let newStory = AEStory(title: title! as String, author: author! as String, article_url: storyURL! as String, pubdate: pubdate! as String, content_without_tags: content_without_tags! as String, picture_url: picture_url! as String);
+                                let newStory = AEStory(title: title! as String, author: author! as String, pubdate: pubdate! as String, content_without_tags: content_without_tags! as String, picture_url: picture_url! as String);
                                 story.append(newStory)
+                                
+                                /*
+                                if let article_key = specificStory["article_id"] as? NSString{
+                                    println(1)
+                                    if let title = specificStory["title"] as? NSString {
+                                        println(2)
+
+                                        if let author = specificStory["author"] as? NSString {
+                                            //let storyURL = specificStory["article_url"] as? NSString
+                                            //let emotion = specificStory["emotion"] as? NSString
+                                            println(3)
+
+                                            if let content_without_tags = specificStory["content_without_tags"] as? NSString {
+                                                println(4)
+
+                                                if let pubdate = specificStory["pubdate"] as? NSString {
+                                                    println(5)
+
+                                                    if let picture_url = specificStory["picture_url"] as? NSString {
+                                                        println(6)
+
+                                                        let newStory = AEStory(title: title as String, author: author as String,  pubdate: pubdate as String, content_without_tags: content_without_tags as String, picture_url: picture_url as String);
+                                                        story.append(newStory)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                } */
                             }
                         }
                     }
@@ -75,8 +100,7 @@ class AEStories: NSObject {
                 dispatch_async(dispatch_get_main_queue(), {
                     completionHandler(self, nil)
                 })
-                //}
-                //}
+
             }
             
         } else {
