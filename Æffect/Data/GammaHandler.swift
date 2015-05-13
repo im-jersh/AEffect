6454c8340338222a5f9c5fad1494330bf7ffb53f
Lawrence Williams <lawgorithm@users.noreply.github.com>
2015-05-13 04:38:45 -0500

    Removed Deprecated Methods in GammaHandler

--- Æffect/Data/GammaHandler.swift
+++ Æffect/Data/GammaHandler.swift
@@ -119,42 +119,4 @@ class GammaHandler: NSObject {
         
     }
     
-    
-    class func processStoryData(data:NSData) {
-        let storyJSON = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
-        var stories = [AEStory]()
-        if let rootDictionary = storyJSON.objectForKey("stories") as? NSArray {
-            for storyDictionary in rootDictionary {
-                let story = AEStory()
-                stories.append(story)
-            }
-        }
-        
-        // archive to disk
-        var error:NSErrorPointer?
-        let mainBundle = NSBundle.mainBundle()
-        // Find the Library directory
-        let libraryURLDirectories:NSArray = NSFileManager.defaultManager().URLsForDirectory(.LibraryDirectory, inDomains: .UserDomainMask)
-        
-        // Get the last object out of this array, this is the URL
-        let libraryURLOptional = libraryURLDirectories.lastObject as? NSURL
-        
-        // If not nil, proceed
-        if let libraryURL = libraryURLOptional {
-            var libraryPath = libraryURL.path?.stringByAppendingPathComponent("AEReadingList.json")
-            NSKeyedArchiver.archiveRootObject(stories, toFile: libraryPath!)
-        }
-        
-        
-    }
-
-    
-    class func loadStoriesFromBundle() -> NSData {
-        var error:NSErrorPointer?
-        let mainBundle = NSBundle.mainBundle()
-        let pathForDataFromBundle = mainBundle.pathForResource("AEReadingList", ofType: "archive")
-        let dataFromBundle = NSFileManager.defaultManager().contentsAtPath(pathForDataFromBundle!)
-        return dataFromBundle!
-    }
-    
 }
